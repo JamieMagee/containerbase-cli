612489504510ea0b38a266f4ac30b97eb2b3ee73
Jamie Magee <jamie.magee@gmail.com>
2023-05-16 21:14:54 -0700

    refactor: remove iife from esbuild script

--- esbuild.ts
+++ esbuild.ts
@@ -1,22 +1,20 @@
 import { build } from "esbuild";
 import { exec } from "pkg";
 
-(async () => {
-  await build({
-    entryPoints: ["src/index.ts"],
-    bundle: true,
-    platform: "node",
-    target: "node18",
-    minify: true,
-    outfile: "./dist/esbuild/containerbase-cli.js",
-  });
+await build({
+  entryPoints: ["src/index.ts"],
+  bundle: true,
+  platform: "node",
+  target: "node18",
+  minify: true,
+  outfile: "./dist/esbuild/containerbase-cli.js",
+});
 
-  await exec([
-    "--targets",
-    "node18-linux-x64,node18-linux-arm64,node18-alpine-x64,node18-alpine-arm64",
-    "--out-path",
-    "./dist/pkg",
-    "--public",
-    "./dist/esbuild/containerbase-cli.js",
-  ]);
-})();
+await exec([
+  "--targets",
+  "node18-linux-x64,node18-linux-arm64,node18-alpine-x64,node18-alpine-arm64",
+  "--out-path",
+  "./dist/pkg",
+  "--public",
+  "./dist/esbuild/containerbase-cli.js",
+]);
